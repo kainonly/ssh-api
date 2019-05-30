c0121b33aae40243850ff91ed89c299ad4529ec8
kain <kainpro@foxmail.com>
2019-05-30 09:35:00 +0800

    Submit

---
+++ index.js
@@ -0,0 +1,11 @@
+const {utils, writeFile} = require('xlsx');
+
+const wb = utils.book_new();
+const ws = utils.json_to_sheet([
+    {A: "S", B: "h", C: "e", D: "e", E: "t", F: "J", G: "S"},
+    {A: 1, B: 2, C: 3, D: 4, E: 5, F: 6, G: 7},
+    {A: 2, B: 3, C: 4, D: 5, E: 6, F: 7, G: 8}
+], {header: ["A", "B", "C", "D", "E", "F", "G"], skipHeader: true});
+utils.book_append_sheet(wb, ws);
+
+writeFile(wb, 'out.xlsx');
