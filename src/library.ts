d514aecd9fee7c5a2e8e87b1494400edd31fa2c0
kain <zhangtqx@vip.qq.com>
2019-05-30 18:07:56 +0800

    Use Typescript Developer

--- src/library.ts
+++ src/library.ts
@@ -1,11 +1,16 @@
-import {utils, writeFile} from 'xlsx';
+import {utils, WorkBook, WorkSheet, write} from 'xlsx';
 
-const wb = utils.book_new();
-const ws = utils.json_to_sheet([
+const workBook: WorkBook = utils.book_new();
+const workSheet: WorkSheet = utils.json_to_sheet([
     {A: 'S', B: 'h', C: 'e', D: 'e', E: 't', F: 'J', G: 'S'},
     {A: 1, B: 2, C: 3, D: 4, E: 5, F: 6, G: 7},
     {A: 2, B: 3, C: 4, D: 5, E: 6, F: 7, G: 8},
 ], {header: ['A', 'B', 'C', 'D', 'E', 'F', 'G'], skipHeader: true});
-utils.book_append_sheet(wb, ws);
 
-writeFile(wb, 'out.xlsx');
+utils.book_append_sheet(workBook, workSheet);
+
+
+const bufferArray = write(workBook, {bookType: 'xlsx', bookSST: false, type: 'array'});
+
+
+console.log(bufferArray);
