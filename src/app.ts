2226c03fe2fabff04726731febcdc2d80970bbbe
kain <kainpro@foxmail.com>
2019-07-12 15:17:13 +0800

    Developer

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import { utils, WorkBook, WorkSheet, write, stream, writeFile } from 'xlsx';
+import { createWriteStream } from 'fs';
+
+const App = () => new Promise(async (resolve) => {
+  const workBook: WorkBook = utils.book_new();
+  const workSheet: WorkSheet = utils.json_to_sheet([
+    { A: 'S', B: 'h', C: 'e', D: 'e', E: 't', F: 'J', G: 'S' },
+    { A: 1, B: 2, C: 3, D: 4, E: 5, F: 6, G: 7 },
+    { A: 2, B: 3, C: 4, D: 5, E: 6, F: 7, G: 8 },
+  ], { header: ['A', 'B', 'C', 'D', 'E', 'F', 'G'], skipHeader: true });
+  utils.book_append_sheet(workBook, workSheet);
+  const arrayBuffer = write(workBook, { bookType: 'xlsx', bookSST: false, type: 'array' });
+  resolve(arrayBuffer);
+});
+
+export { App };
