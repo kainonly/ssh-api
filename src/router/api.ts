e5e9518e976b8d8eed0db70abf6c40fc707458b5
kain <zhangtqx@vip.qq.com>
2019-09-29 10:19:01 +0800

    Add JSON Schema

--- src/router/api.ts
+++ src/router/api.ts
@@ -4,55 +4,220 @@ const api = (fastify: FastifyInstance) => {
   /**
    * Lists all ssh client
    */
-  fastify.post('/lists', (request, reply) => {
+  fastify.post('/lists', {
+    schema: {
+      body: {
+        required: ['identity'],
+        properties: {
+          identity: {
+            type: 'array',
+          },
+        },
+      },
+    },
+  }, (request, reply) => {
     reply.send({ error: 0 });
   });
   /**
    * Get a ssh client
    */
-  fastify.post('/get', (request, reply) => {
+  fastify.post('/get', {
+    schema: {
+      body: {
+        required: ['identity'],
+        properties: {
+          identity: {
+            type: 'string',
+          },
+        },
+      },
+    },
+  }, (request, reply) => {
     reply.send({ error: 0 });
   });
   /**
    * Testing a ssh client
    */
-  fastify.post('/testing', (request, reply) => {
+  fastify.post('/testing', {
+    schema: {
+      body: {
+        required: ['host', 'port', 'username'],
+        properties: {
+          host: {
+            type: 'string',
+          },
+          port: {
+            type: 'number',
+          },
+          username: {
+            type: 'string',
+          },
+          password: {
+            type: 'string',
+          },
+          private_key: {
+            type: 'string',
+          },
+          passphrase: {
+            type: 'string',
+          },
+        },
+        oneOf: [
+          {
+            required: ['password'],
+          },
+          {
+            required: ['private_key'],
+          },
+        ],
+      },
+    },
+  }, (request, reply) => {
+    const body = request.body;
+    console.log(body);
     reply.send({ error: 0 });
   });
   /**
    * Put a ssh client
    */
-  fastify.post('/put', (request, reply) => {
+  fastify.post('/put', {
+    schema: {
+      body: {
+        required: ['identity', 'host', 'port', 'username'],
+        properties: {
+          identity: {
+            type: 'string',
+          },
+          host: {
+            type: 'string',
+          },
+          port: {
+            type: 'number',
+          },
+          username: {
+            type: 'string',
+          },
+          password: {
+            type: 'string',
+          },
+          private_key: {
+            type: 'string',
+          },
+          passphrase: {
+            type: 'string',
+          },
+        },
+        oneOf: [
+          {
+            required: ['password'],
+          },
+          {
+            required: ['private_key'],
+          },
+        ],
+      },
+    },
+  }, (request, reply) => {
     reply.send({ error: 0 });
   });
   /**
    * Delete a ssh client
    */
-  fastify.post('/delete', (request, reply) => {
+  fastify.post('/delete', {
+    schema: {
+      body: {
+        required: ['identity'],
+        properties: {
+          identity: {
+            type: 'string',
+          },
+        },
+      },
+    },
+  }, (request, reply) => {
     reply.send({ error: 0 });
   });
   /**
    * A ssh client to exec
    */
-  fastify.post('/exec', (request, reply) => {
+  fastify.post('/exec', {
+    schema: {
+      body: {
+        required: ['identity', 'bash'],
+        properties: {
+          identity: {
+            type: 'string',
+          },
+          bash: {
+            type: 'string',
+          },
+        },
+      },
+    },
+  }, (request, reply) => {
     reply.send({ error: 0 });
   });
   /**
    * Create a tunnel
    */
-  fastify.post('/bind', (request, reply) => {
+  fastify.post('/bind', {
+    schema: {
+      body: {
+        required: ['identity', 'scr_ip', 'src_port', 'dst_port'],
+        properties: {
+          identity: {
+            type: 'string',
+          },
+          src_ip: {
+            type: 'string',
+          },
+          src_port: {
+            type: 'number',
+          },
+          dst_port: {
+            type: 'number',
+          },
+        },
+      },
+    },
+  }, (request, reply) => {
     reply.send({ error: 0 });
   });
   /**
    * Delete a tunnel
    */
-  fastify.post('/unbind', (request, reply) => {
+  fastify.post('/unbind', {
+    schema: {
+      body: {
+        required: ['identity', 'dst_port'],
+        properties: {
+          identity: {
+            type: 'string',
+          },
+          dst_port: {
+            type: 'number',
+          },
+        },
+      },
+    },
+  }, (request, reply) => {
     reply.send({ error: 0 });
   });
   /**
    * Get tunnels of ssh client
    */
-  fastify.post('/listen', (request, reply) => {
+  fastify.post('/listen', {
+    schema: {
+      body: {
+        required: ['identity'],
+        properties: {
+          identity: {
+            type: 'string',
+          },
+        },
+      },
+    },
+  }, (request, reply) => {
     reply.send({ error: 0 });
   });
 };
