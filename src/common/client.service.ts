50c77f390077a28ea2f78eb6836f6b6d333f8335
kain <kainpro@foxmail.com>
2019-09-29 11:13:24 +0800

    Connected Testing

--- src/common/client.service.ts
+++ src/common/client.service.ts
@@ -1,9 +1,24 @@
 import { ClientOption } from '../types/client-option';
+import { Client, ConnectConfig } from 'ssh2';
 
 export class ClientService {
   private client: ClientOption;
 
-  testing() {
-
+  testing(config: ConnectConfig): Promise<any> {
+    return new Promise((resolve, reject) => {
+      let client = new Client();
+      client.connect(config);
+      client.on('ready', () => {
+        resolve('ok');
+        client.destroy();
+      });
+      client.on('error', error => {
+        reject(error);
+      });
+      client.on('close', () => {
+        client.removeAllListeners();
+        client = undefined;
+      });
+    });
   }
 }
