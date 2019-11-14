422d16d17b55e9702f0e73348d68ef1d1c87aa9f
kain <zhangtqx@vip.qq.com>
2019-11-14 17:57:05 +0800

    Update Config

--- common/config.go
+++ common/config.go
@@ -1,14 +1,31 @@
 package common
 
 type Config struct {
-	FilePath string
-	Value    map[string]string
+	filePath string
+	value    map[string]SSHOption
 }
 
-func (config *Config) Get() map[string]string {
-	return config.Value
+type SSHOption struct {
+	Host       string `json:"host"`
+	Port       uint   `json:"port"`
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+	PrivateKey string `json:"private_key"`
+	Passphrase string `json:"passphrase"`
 }
 
-func (config *Config) Set(value map[string]string) {
-	config.Value = value
+func (config *Config) init() {
+	if config.value == nil {
+		config.value = make(map[string]SSHOption)
+	}
+}
+
+func (config *Config) Get(identity string) (exists bool, options SSHOption) {
+	exists = config.value[identity] != (SSHOption{})
+	options = config.value[identity]
+	return
+}
+
+func (config *Config) Set(identity string, value *SSHOption) {
+	config.value[identity] = *value
 }
