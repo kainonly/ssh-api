65a0d24d841c6c418c16cbd92db55b4ab02a831c
kain <zhangtqx@vip.qq.com>
2019-11-22 18:19:21 +0800

    Test Travis CI

--- scripts/ci-tag.sh
+++ scripts/ci-tag.sh
@@ -4,6 +4,6 @@ echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdi
 # Build Golang Application
 go build -o dist/ssh-api
 # Build docker image
-docker build . -t ssh-api
+docker build . -t kainonly/ssh-api:${TRAVIS_TAG}
 # Push docker image
 docker push kainonly/ssh-api:${TRAVIS_TAG}
